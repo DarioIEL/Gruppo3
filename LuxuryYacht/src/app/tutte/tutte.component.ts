0d85f3a40e145cbccb2f518d779c8143bae4cb17
Simo <96527499+Simoo92@users.noreply.github.com>
2022-05-06 15:07:58 +0200

    aggiornamento

--- LuxuryYacht/src/app/tutte/tutte.component.ts
+++ LuxuryYacht/src/app/tutte/tutte.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { yachtService } from 'src/service/yacht.service';
 import { formService } from '../form/form.service';
 import { Yacht } from '../model/yacht';
 
@@ -14,12 +15,19 @@ export class TutteComponent implements OnInit {
   yachts = [];
 
   constructor(private formService: formService,
+              private yachtService: yachtService,
               private http: HttpClient) { }
 
   ngOnInit(): void {
     this.formService.getYacht().subscribe(data => {
       this.yachts = data;
     })
+    this.yachtService.getOneYacht().subscribe(data =>{
+      this.yachts = data;
+      console.log(data);
+      
+    })
+
   }
 
   
--- LuxuryYacht/src/service/yacht.service.ts
+++ LuxuryYacht/src/service/yacht.service.ts
@@ -1,42 +1,94 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { map } from "rxjs";
-import { Yacht } from "src/app/model/yacht";
+import {
+  HttpClient
+} from "@angular/common/http";
+import {
+  Injectable
+} from "@angular/core";
+import {
+  map
+} from "rxjs";
+import {
+  Yacht
+} from "src/app/model/yacht";
 
 @Injectable({
 
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
-export class yachtService{
+export class yachtService {
 
-constructor(private http: HttpClient){
+  
 
-}
+    constructor(private http: HttpClient) {
 
-getOfferta(){
+    }
+
+
+
+  getOfferta() {
     return this.http
-    .get('https://luxuryyacht-59154-default-rtdb.europe-west1.firebasedatabase.app/yacht.json')
-    .pipe(map(response =>{
-        const yachtsOfferta: Yacht[] = [];
+      .get('https://luxuryyacht-59154-default-rtdb.europe-west1.firebasedatabase.app/yacht.json')
+      .pipe(map(response => {
+          const yachtsOfferta: Yacht[] = [];
 
-        
-        for(let key in response){
-            if (response[key].offerta == "Si"){
-                yachtsOfferta.push((response[key]));
-                console.log((response[key]).offerta);
+
+          for (let key in response) {
+            if (response[key].offerta == "Si") {
+              yachtsOfferta.push((response[key]));
+              console.log((response[key]).offerta);
 
             }
+          }
+
+
+          return yachtsOfferta;
         }
 
+      ))
+  }
 
-        return yachtsOfferta;
-    }
-    
-    ))
+
+  getOneYacht() {
+    return this.http
+      .get < {
+        [key: string]: Yacht
+      } > ('https://luxuryyacht-59154-default-rtdb.europe-west1.firebasedatabase.app/yacht.json')
+      .pipe(map(responseData => {
+        const yachtArray: Yacht[] = [];
+
+        for (let key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            yachtArray.push({
+              ...responseData[key],
+              id: key
+            });
+          }
+          console.log(yachtArray);
+        }
+
+        return yachtArray;
+        
+      }))
+  }
+
+
+
+  // getViaggi(){
+  //     return this.http
+  //            .get< {[key: string]: Viaggi} > ('https://viaggitss-default-rtdb.europe-west1.firebasedatabase.app/mieiViaggi.json')
+  //            .pipe(map(responseData => {
+  //              const viaggiArray: Viaggi[] = [];
+
+  //              for(let key in responseData){
+  //                if(responseData.hasOwnProperty(key)){
+  //                feedArray.push({...responseData[key], id: key});
+  //                }
+  //              }
+  //              return viaggiArray
+  //            }))
+  //  }
+
+
+
 }
-
-
-
-}
-
